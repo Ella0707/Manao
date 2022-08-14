fa44f8947b093131c835c07f062b471a1a62d486
Ella0707 <pakelvi77@gmail.com>
2022-08-14 21:44:31 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -15,20 +15,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (error === 0) {
       popupOpen();
-  
-      const name = formData.get('name'); 
+
+      const name = formData.get('name');
       const email = formData.get('email');
       const textarea = formData.get('description');
 
       var checkboxes = document.getElementsByClassName('checkbox');
-          var checkboxesChecked = []; 
-          for (var index = 0; index < checkboxes.length; index++) {
-            if (checkboxes[index].checked) {
-                checkboxesChecked.push(checkboxes[index].value); 
-            }
-          }
+      var checkboxesChecked = [];
+      for (var index = 0; index < checkboxes.length; index++) {
+        if (checkboxes[index].checked) {
+          checkboxesChecked.push(checkboxes[index].value);
+        }
+      }
 
-      console.log(name,email,textarea,checkboxesChecked); //вывод в консоль
+      console.log(name, email, textarea, checkboxesChecked); //вывод в консоль
       // formPreview.innerHTML = " ";
       // form.reset();
 
@@ -66,28 +66,80 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function popupOpen() {
-    popup.classList.add('active'); 
+    popup.classList.add('active');
   }
 })
 
 
-
 const menuBtn = $(".btn"),
-      menu = $(".popup");
+  menu = $(".popup");
 
 menuBtn.on("click", function () {
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        menu.slideUp();
-    } else {
-        $(this).addClass("active");
-        menu.slideDown();
-    }
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    menu.slideUp();
+  } else {
+    $(this).addClass("active");
+    menu.slideDown();
+  }
 });
 
 $(document).click(function (e) {
-    if (!menuBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
-        menu.slideUp();
-        menuBtn.removeClass("active");
-    };
+  if (!menuBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
+    menu.slideUp();
+    menuBtn.removeClass("active");
+  };
 });
+
+
+const publicationsSlider = new Swiper('.publications__slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  speed: 800,
+  navigation: {
+    prevEl: '.slider-arrow-prev',
+    nextEl: '.slider-arrow-next',
+  },
+
+  breakpoints: {
+
+    769: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+
+// мобильное меню
+const iconMenu = document.querySelector('.menu-btn');
+const menuMob = document.querySelector('.header__nav');
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuMob.classList.toggle('active');
+  });
+}
+
+// плавная прокрутка
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault()
+    let blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+
+    })
+    if (iconMenu.classList.contains('active')) {
+      document.body.classList.remove('lock');
+      iconMenu.classList.remove('active');
+      menuMob.classList.remove('active');
+    }
+  })
+}
